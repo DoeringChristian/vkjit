a2e5e42501de467112a0ac30d602fa05ae7f0f9c
Döring Christian <christian.doering@um.de>
2023-02-21 21:37:19 +0100

    macro_rules

--- libs/vkjit-python/src/types.rs
+++ libs/vkjit-python/src/types.rs
@@ -52,89 +52,35 @@ macro_rules! var {
                 pub fn tolist(&self) -> Vec<$ty>{
                     Vec::from(IR.lock().unwrap().as_slice::<$ty>(self.0))
                 }
-                $(method_bops!{ $ty, $bop })*
+
+                // Add binary ops
+                $(
+                    pub fn [<__ $bop __>](&self, rhs: &Self) -> Self {
+                        Self(IR.lock().unwrap().$bop(self.0, rhs.0))
+                    }
+                )*
             }
         }
     };
 }
 
-// var! {
-//     impl i32{
-//         bop{
-//             add;
-//             sub;
-//             mul;
-//             div;
-//         }
-//     }
-// }
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct F32(VarId);
-#[pymethods]
-impl F32 {
-    #[new]
-    pub fn __new__(src: &PyAny) -> PyResult<Self> {
-        if let Ok(val) = src.extract::<f32>() {
-            return Ok(Self(IR.lock().unwrap().const_f32(val)));
+var! {
+    impl f32{
+        bop{
+            add;
+            sub;
+            mul;
+            div;
         }
-        if let Ok(val) = src.extract::<Vec<f32>>() {
-            return Ok(Self(IR.lock().unwrap().array_f32(&val)));
-        }
-        Err(PyTypeError::new_err("Not a valid argument!"))
-    }
-    pub fn id(&self) -> usize {
-        self.0.get_id()
-    }
-    pub fn tolist(&self) -> Vec<f32> {
-        Vec::from(IR.lock().unwrap().as_slice::<f32>(self.0))
-    }
-    pub fn __add__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().add(self.0, rhs.0))
-    }
-    pub fn __sub__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().sub(self.0, rhs.0))
-    }
-    pub fn __mul__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().mul(self.0, rhs.0))
-    }
-    pub fn __div__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().div(self.0, rhs.0))
     }
 }
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct I32(VarId);
-#[pymethods]
-impl I32 {
-    #[new]
-    pub fn __new__(src: &PyAny) -> PyResult<Self> {
-        if let Ok(val) = src.extract::<i32>() {
-            return Ok(Self(IR.lock().unwrap().const_i32(val)));
+var! {
+    impl i32{
+        bop{
+            add;
+            sub;
+            mul;
+            div;
         }
-        if let Ok(val) = src.extract::<Vec<i32>>() {
-            return Ok(Self(IR.lock().unwrap().array_i32(&val)));
-        }
-        Err(PyTypeError::new_err("Not a valid argument!"))
-    }
-    pub fn id(&self) -> usize {
-        self.0.get_id()
-    }
-    pub fn tolist(&self) -> Vec<i32> {
-        Vec::from(IR.lock().unwrap().as_slice::<i32>(self.0))
-    }
-    pub fn __add__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().add(self.0, rhs.0))
-    }
-    pub fn __sub__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().sub(self.0, rhs.0))
-    }
-    pub fn __mul__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().mul(self.0, rhs.0))
-    }
-    pub fn __div__(&self, rhs: &Self) -> Self {
-        Self(IR.lock().unwrap().div(self.0, rhs.0))
     }
 }
